35be85524ad39fc7e9c75b3db41b2549c61201d8
Viktar <100156124+YakimchukViktar@users.noreply.github.com>
2022-04-26 16:34:54 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("3");
         System.out.println("Вас приветствует генератор паролей!");
-        System.out.print(5);
         System.out.print("Укажите длину пароля (количество символов)? ");
 
     }
