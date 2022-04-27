1ce22b2ae354e3260dcb54c077812df3f0e9399f
Yakimchuk Viktar <yakimchukviktar@gmail.com>
2022-04-27 10:56:04 +0300

    10

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,18 @@ public class Main {
         System.out.print("Укажите длину пароля (количество символов)? ");
         int lengthPassword = getUserNumber();// вызываем метод, который возвратит нам длину пароля
         while (true) {
-
             if (lengthPassword < 5) { //проверка пароля на нужную нам длину
                 lengthPassword = getUserNumber();
-
-           
             } else break;
         }
         System.out.println("Your password: ");
-        for (int i = 5; i < lengthPassword+5; i++) { //в этой строке 0 изменил на 5 и добавил 5 к длине
+        for (int i = 5; i < lengthPassword + 5; i++) { //в этой строке 0 изменил на 5 и добавил 5 к длине
             System.out.print((char) (short) (Math.random() * 94 + 33));
         }
     }
 
     private static int getUserNumber() {
-        System.out.print("Введите целое число не менее 8: ");//длина пароля сейчас не меньше 8
+        System.out.print("Введите целое число не менее 5: ");//длина пароля сейчас не меньше 5
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNextInt()) {
             return scanner.nextInt();
